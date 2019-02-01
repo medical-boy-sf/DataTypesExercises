14ee206b5749315177a23c1479af464fd3356ec2
medical-boy-sf <medical_boy_sf@yahoo.com>
2019-02-01 11:06:34 +0200

    02.charToString.js

--- 01.sumDigits.js
+++ 01.sumDigits.js
@@ -1,3 +1,12 @@
 function sumDigits(number) {
+    number = Number(number);
+    let sum = 0;
 
+    while (number > 0) {
+        sum += Math.trunc(number % 10);
+        number /= 10;
+    }
+    console.log(sum);
 }
+
+sumDigits(17);
